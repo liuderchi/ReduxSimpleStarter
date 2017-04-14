eced41ffba698b8e1cc57c63d7c8ba19dd3af0a4
Derek Liu <derek.liu@tw.promise.com>
2017-04-14 11:32:21 +0800

    comment for index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from 'react';   // dependencies for JSX
 import ReactDOM from 'react-dom';
 
+// create an functional component App
 const App = () => {
   return (
     <div>Hello</div>
@@ -8,4 +9,5 @@ const App = () => {
   // NOTE JSX is transpiled into React.createElement(...)
 }
 
+// render component into DOM with specified DOM target
 ReactDOM.render(<App />, document.querySelector('.container'))
