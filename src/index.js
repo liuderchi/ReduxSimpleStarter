b5ef9cf5a92d1ba4709421d59035d9dc65246814
liuderchi <liuderchi@gmail.com>
2017-04-15 22:41:05 +0800

    lec 22: refactor App to class based component
    - search youtube in constructor, set videos to this.state.videos

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyCsXdbPrzBFmlIYc017K28qw6LiH8Hm-iM'
 
-// hit youtube API
-YTSearch({key: API_KEY, term: 'nba playoff'}, (data) => {
-  console.log(data);
-})
-
 // create an functional component App
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-  // NOTE JSX is transpiled into React.createElement(...)
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'nba playoff'}, videos => {
+      this.setState({ videos })
+      // this.setState({ videos: videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+
 }
 
 // render component into DOM with specified DOM target
