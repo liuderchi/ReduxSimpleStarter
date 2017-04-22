28a5622f63461fad34d3627e774efb309cad2ab5
liuderchi <liuderchi@gmail.com>
2017-04-22 22:56:03 +0800

    lec 49, 50: handle init state rendering of BookDetail
    - try add more schema on 'books' state

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    { title: 'Harry Potter'},
-    { title: 'JavaScript: the good parts'},
-    { title: 'Pride and Prejudice'},
-    { title: 'Dark Tower'},
+    { title: 'Harry Potter', pages: 123 },
+    { title: 'JavaScript: the good parts', pages: 456},
+    { title: 'Pride and Prejudice', pages: 789},
+    { title: 'Dark Tower', pages: 987},
   ];
 };
