fc80c2e19842b3b7f1c6dea303abee49c4ae6a9f
liuderchi <liuderchi@gmail.com>
2017-04-22 17:28:12 +0800

    lec 46: add action callback in container
    - return an action object in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
 export function selectBook(book) {
   // called when action occurs
-  console.log(`A book has been selected: ${book.title}`);
+
+  // should return an action obj with prop: type, payload
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+    // NOTE payload is critical object passed on to reducers
+  };
 }
--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -8,6 +8,7 @@ class BookList extends Component {
     return this.props.books.map((book) => {
       return (
         <li key={book.title}
+            onClick={() => {this.props.selectBook(book)}}
             className="list-group-item">
               {book.title}
         </li>
