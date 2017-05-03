c6ee76458d2b4ac06d06f44f0c70cba29c505833
liuderchi <liuderchi@gmail.com>
2017-05-03 22:57:10 +0800

    lec 118 119: POC of BrowserRouter and Route
    - remove App component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Myhello extends React.Component {
+  render() { return (<div>hello</div>) }
+}
+class Mybyebye extends React.Component {
+  render() { return (<div>byebye</div>) }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Myhello} />
+        <Route path="/byebye" component={Mybyebye} />
+      </div>
+    </ BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
