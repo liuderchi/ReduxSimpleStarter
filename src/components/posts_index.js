7fcff87bb17dee211ff93a968c7d195d499c51cc
liuderchi <liuderchi@gmail.com>
2017-05-08 15:18:35 +0800

    lec 126: bind fetchPosts action with component posts_index.js using connect()
    - use shortcut rather than mapDispatchToProps() with connect

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>This is Posts index page!</div>
@@ -8,4 +14,5 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
+// NOTE we don't use mapDispatchToProps() since adding dispatch was just a more flexible API usage
