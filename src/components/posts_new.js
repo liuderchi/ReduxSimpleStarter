9ae0083864a8f7bac963fe126ecdcee43db74a62
liuderchi <liuderchi@gmail.com>
2017-05-09 16:20:23 +0800

    lec 132, 133: adding Field to redux form
    - use redux form to handle submission and validation
    
    - importing Field, reduxForm
    - jsx: form>Field
    - use reduxForm() to wrap component
    - pass fcn renderField to Field
    - fixup error message by update react-redux to v4.4.8:
        - npm update react-redux --save

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...(field.input)}
+        />
+      {/* NOTE ...field.input
+        Object Spread Properties syntax
+          e.g. var obj = { id: 5, ...field.input})
+      */}
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div>PostsNew!</div>
+      <form>
+        <Field
+          name="title"
+          component={this.renderField}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({   // passing option obj
+  form: 'PostsNewForm'    // arbitrary string for form identity
+})(PostsNew);
